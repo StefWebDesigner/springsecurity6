ee761fec66ef8dec7bdefef3fa70b0f5cc8fa3d7
Stefan <stefan_riley@icloud.com>
2025-03-19 12:44:49 -0400

    new link

--- src/main/java/com/example/springsecurity6/security/SecurityConfig.java
+++ src/main/java/com/example/springsecurity6/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.springsecurity6.security;
 public class SecurityConfig {
 
     //UUSe this!
-    https://github.com/navinreddy20/spring6yt/blob/main/Part37-Spring%20Security%206%20Generating%20JWT%20Token/src/main/java/com/telusko/part29springsecex/config/SecurityConfig.java
+
+
+    https://github.com/navinreddy20/spring6yt/tree/main/Part38-Spring%20Security%206%20Validating%20JWT%20Token/src/main/java/com/telusko/part29springsecex
 
 }
