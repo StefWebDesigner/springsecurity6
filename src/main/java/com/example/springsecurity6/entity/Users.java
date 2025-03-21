4aad615882e38c41d2f382252b04252f8c9f49df
Stefan <stefan_riley@icloud.com>
2025-03-21 12:23:35 -0400

    fixed lombok issue

--- src/main/java/com/example/springsecurity6/entity/Users.java
+++ src/main/java/com/example/springsecurity6/entity/Users.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
-//@Data
+@Data
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,36 +17,36 @@ public class Users {
     private String username;
     private String password;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Users{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Users{" +
+//                "id=" + id +
+//                ", username='" + username + '\'' +
+//                ", password='" + password + '\'' +
+//                '}';
+//    }
 }
