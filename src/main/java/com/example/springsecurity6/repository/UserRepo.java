84370d382b128734fc86ef5b1123086f996c88c0
StefWebDesigner <stefan_riley@icloud.com>
2025-03-21 03:47:15 -0400

    added another spring security

---
+++ src/main/java/com/example/springsecurity6/repository/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.springsecurity6.repository;
+
+
+import com.example.springsecurity6.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
---
+++ src/main/java/com/example/springsecurity6/service/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.springsecurity6.service;
+
+
+import com.example.springsecurity6.entity.UserPrincipal;
+import com.example.springsecurity6.entity.Users;
+import com.example.springsecurity6.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
